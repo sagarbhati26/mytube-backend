dec651e415ce21b595d6f375651bb6fb972d0c6f
sagarbhati26 <sagarbhati2616@gmail.com>
2024-11-05 15:15:57 +0530

    register route

--- src/app.js
+++ src/app.js
@@ -12,4 +12,12 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declare
+app.use("/api/v1/users",userRouter)
+
+
 export{app}
