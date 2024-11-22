704f15e64f7f15262ec757b6e7082b64751a1240
sagarbhati26 <sagarbhati2616@gmail.com>
2024-11-23 01:42:15 +0530

    logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -101,7 +101,27 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async(req,res)=>{
-     
+    await User.findByIdAndUpdate(
+        req.body._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+     )
+     const options = {
+        httpOnly: true,
+        secure: true
+        
+    };
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200),{},"user logged out")
 })
 
-export { registerUser, loginUser };
+export { registerUser, loginUser,logoutUser };
